dfc9351a22d2fe05db9b8167ab7bde6f27311401
Naren Krishnaa <narenkrishnaar@gmail.com>
2024-07-28 00:09:01 +0530

    Added CustomTextField widget for reusable text field component

---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+final String hintText;
+final TextEditingController controller;
+final TextInputType keyboardType;
+final bool obscureText;
+final String? Function(String?)? validator;
+
+const CustomTextField({
+super.key,
+required this.hintText,
+required this.controller,
+this.keyboardType = TextInputType.text,
+this.obscureText = false,
+this.validator,
+});
+
+@override
+Widget build(BuildContext context) {
+return TextFormField(
+  controller: controller, 
+  keyboardType: keyboardType, 
+  obscureText: obscureText, 
+  decoration: InputDecoration(
+    hintText: hintText, 
+    fillColor: const Color(0xffF0F0F0),
+    filled: true,
+    border: OutlineInputBorder(
+      borderSide: const BorderSide(
+        color:  Color(0xff4B3B2E), 
+        width: 1.0) ,
+      borderRadius: BorderRadius.circular(8.0))));
+}
+}
