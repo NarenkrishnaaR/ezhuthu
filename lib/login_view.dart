054e2f875fef33285989b6b64e516335ec255240
Naren Krishnaa <narenkrishnaar@gmail.com>
2024-07-28 00:09:49 +0530

    Create a LoginView with Email, Password textfields and Login button

---
+++ lib/login_view.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:ezhuthu/widgets/custom_text_field.dart';
+import 'package:ezhuthu/widgets/custom_button.dart';
+
+class LoginView extends StatefulWidget {
+const LoginView({super.key});
+@override
+LoginViewState createState() => LoginViewState();
+}
+
+class LoginViewState extends State<LoginView> {
+final TextEditingController emailController = TextEditingController();
+final TextEditingController passwordController = TextEditingController();
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+  backgroundColor: Colors.white,
+  body: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const SizedBox(height: 32),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32),
+        child: Align(alignment: Alignment.center,child:Image.asset('assets/images/logo.png'))),
+        const SizedBox(height: 24),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32), 
+        child: CustomTextField(hintText: "E-mail", controller: emailController),
+        ),
+        const SizedBox(height: 16),
+          Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32), 
+        child: CustomTextField(hintText: "Password", controller: passwordController, obscureText: true)
+        ),
+        const SizedBox(height: 24),
+          Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32), 
+        child: SizedBox(
+          height: 50, 
+          width: double.infinity,
+        child: CustomButton(
+          title: "Login", 
+          onPressed: () {
+        })
+        ) 
+        )
+      ]
+    ),
+);
+}
+}
