73de77a57d1bac90802462d223bb3c0d24686ea8
Naren Krishnaa <narenkrishnaar@gmail.com>
2024-07-29 21:11:03 +0530

    Separate widget is created for textfields and button

--- lib/login_view.dart
+++ lib/login_view.dart
@@ -14,38 +14,51 @@ final TextEditingController passwordController = TextEditingController();
 @override
 Widget build(BuildContext context) {
 return Scaffold(
-  backgroundColor: Colors.white,
-  body: Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        const SizedBox(height: 32),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 32),
-        child: Align(alignment: Alignment.center,child:Image.asset('assets/images/logo.png'))),
-        const SizedBox(height: 24),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 32), 
-        child: StyledTextField(hintText: "E-mail", controller: emailController),
-        ),
-        const SizedBox(height: 16),
-          Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 32), 
-        child: StyledTextField(hintText: "Password", controller: passwordController, obscureText: true)
-        ),
-        const SizedBox(height: 24),
-          Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 32), 
-        child: SizedBox(
-          height: 50, 
-          width: double.infinity,
-        child: StyledButton(
-          title: "Login", 
-          onPressed: () {
-        })
-        ) 
-        )
-      ]
-    ),
+backgroundColor: Colors.white,
+body: Column(
+crossAxisAlignment: CrossAxisAlignment.center,
+children: [
+  const SizedBox(height: 32),
+  Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 32),
+  child: Align(alignment: Alignment.center,child:Image.asset('assets/images/logo.png'))
+  ),
+  const SizedBox(height: 24),
+  configureLoginTextfields(),
+  const SizedBox(height: 24),
+  configureLoginButton()
+  ]) 
+  );
+}
+
+Widget configureLoginTextfields() {
+return Column(
+children: [
+Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 32), 
+  child: StyledTextField(hintText: "E-mail", controller: emailController),
+  ),
+  const SizedBox(height: 16),
+    Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 32), 
+  child: StyledTextField(hintText: "Password", controller: passwordController, obscureText: true)
+  ) 
+]
+);
+}
+
+Widget configureLoginButton() {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 32), 
+  child: SizedBox(
+    height: 50, 
+    width: double.infinity,
+  child: StyledButton(
+    title: "Login", 
+    onPressed: () {
+    }
+  )
+  )
 );
 }
 }
