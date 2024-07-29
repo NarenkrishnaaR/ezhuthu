473ef551ff49488090cdb1834328e40d75c11168
Naren Krishnaa <narenkrishnaar@gmail.com>
2024-07-29 23:31:31 +0530

    Modified secondary color

--- lib/widgets/app_colors.dart
+++ lib/widgets/app_colors.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primary = Color(0xFF4B3B2E); 
-  static const Color secondary = Color(0xFF8D6E63);
+  static const Color secondary = Color(0xFFDAA520);
   static const Color background = Color(0xFFFFFFFF);
   static const Color text = Color(0xFF000000);
   static const Color gray = Color(0xffF0F0F0);
